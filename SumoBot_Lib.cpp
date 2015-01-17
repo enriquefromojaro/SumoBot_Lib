3473c3d2b07af75eb70bc8c1c8ff875f79f49a0f
enriquefromojaro <enriquefromojaro@gmail.com>
2015-01-17 19:45:04 +0100

    SumoBot_Lib
    Versión 1.2

--- SumoBot_Lib.cpp
+++ SumoBot_Lib.cpp
@@ -3,6 +3,29 @@
 #include "arduino.h"
 #define COEFICIENTE 16746.73
 #define EXPONENTE -1.2134
+
+
+///-----------------------------------SumoBot------------------------------------------------------------------------------
+
+//---Builder
+
+SumoBot::SumoBot()
+{
+	//Asignamos los pines como indica el manual
+	pinDist=A3;
+	pinLight_R=A0;
+	pinLight_L=A1;
+	pinMotor_R1=5;
+	pinMotor_R2=6;
+	pinMotor_L1=6;
+	pinMotor_L2=7;
+	pinRed=2;
+	pinGreen=3;
+	pinBlue=4;
+	
+	//Llamamos a losconstructores de los componentes para inicializarlos
+
+}
 float SumoBot::getDistance(float voltage)//Retorna la distancia en centímetros (cm) según la formula del SHARP en función del voltaje introducido
 {
 	return COEFICIENTE * float(pow(voltage,EXPONENTE));
@@ -14,22 +37,44 @@ DistanceSensor::DistanceSensor(int pinInput)//Constructor del Sensor de Distanci
 {
 	pin=pinInput;
 	pinMode(pin, INPUT);
-
+	coeficiente= COEFICIENTE;
+	exponente= EXPONENTE;
 }
 float DistanceSensor::distance()//Retorna la distancia en centímetros (cm) según la formula del SHARP
 {
-	return COEFICIENTE * float(pow(analogRead(pin),EXPONENTE));
+	return coeficiente * float(pow(analogRead(pin),exponente));
 }
 
-float DistanceSensor::analogVoltage()
+float DistanceSensor::anRead()
 {
 	return analogRead(pin);
 }
 
-float DistanceSensor::digitalVoltage()
+float DistanceSensor::digRead()
 {
 	return digitalRead(pin);
 }
+
+void DistanceSensor::setCoeficiente(float coef)
+{
+	coeficiente=coef;
+}
+
+void DistanceSensor::setExponente(float exp)
+{
+	exponente=exp;
+}
+
+float DistanceSensor::getCoeficiente()
+{
+	return coeficiente;
+}
+
+float DistanceSensor::getExponente()
+{
+	return exponente;
+}
+
 
 //--------------------------LightSensor-----------------------------------------------------
 LightSensor::LightSensor(int pinInput)//Constructor del Sensor de luz. LLeva como parámetro el pin en el que está
@@ -44,12 +89,12 @@ boolean LightSensor::black()//retorna true si el color es negro y false si no
 	return HIGH==digitalRead(pin);
 }
 
-float LightSensor::analogVoltage()
+float LightSensor::anRead()
 {
 	return analogRead(pin);
 }
 
-float LightSensor::digitalVoltage()
+float LightSensor::digRead()
 {
 	return digitalRead(pin);
 }
@@ -83,7 +128,7 @@ void Motor::forward()//Avanzar
 	digitalWrite(pin[1], HIGH);
 }
 
-void Motor::backward()//Retroeder
+void Motor::backward()//Retroceder
 {
 	digitalWrite(pin[0], HIGH);
 	digitalWrite(pin[1], LOW);
@@ -93,4 +138,56 @@ void Motor::stop()//parar
 {
 	digitalWrite(pin[0], LOW);
 	digitalWrite(pin[1], LOW);
+}
+
+
+//------------------------------------------Led------------------------------------------------------------------
+
+//Builder
+Led::Led(int red, int green, int blue)
+{
+	pinRed=red;
+	pinGreen=green;
+	pinBlue=blue;
+	
+	//Inicializamos los pines a salida
+	pinMode(pinRed, OUTPUT);
+	pinMode(pinGreen, OUTPUT);
+	pinMode(pinBlue, OUTPUT);
+}
+
+//Method red---> Turns on the led with red color
+void Led::red()
+{
+	//Ponemos todos los pines a LOW excepto el rojo (pinRed)
+	digitalWrite(pinRed, HIGH);
+	digitalWrite(pinBlue, LOW);
+	digitalWrite(pinGreen, LOW);
+}
+
+//Method blue---> Turns on the led with blue color
+void Led::blue()
+{
+	//Ponemos todos los pines a LOW excepto el azul (pinBlue)
+	digitalWrite(pinRed, LOW);
+	digitalWrite(pinBlue, HIGH);
+	digitalWrite(pinGreen, LOW);
+}
+
+//Method green---> Turns on the led with green color
+void Led::green()
+{
+	//Ponemos todos los pines a LOW excepto el verde (pinGreen)
+	digitalWrite(pinRed, LOW);
+	digitalWrite(pinBlue, LOW);
+	digitalWrite(pinGreen, HIGH);
+}
+
+//Method red---> Turns off the led
+void Led::off()
+{
+	//Ponemos todos los pines a LOW
+	digitalWrite(pinRed, LOW);
+	digitalWrite(pinBlue, LOW);
+	digitalWrite(pinGreen, LOW);
 }
--- SumoBot_Lib.h
+++ SumoBot_Lib.h
@@ -6,18 +6,30 @@
 #include "arduino.h"
 class SumoBot
 {
+private:
+	int pinDist, pinLight_R, pinLight_L, pinMotor_R1, pinMotor_R2, pinMotor_L1, pinMotor_L2, pinRed, pinGreen, pinBlue;
+	/*DistanceSensor dist;
+	LightSensor lightR, lightL;
+	Motor motorR, motorL;
+	Led led*/
 public:
+	SumoBot();
 	float getDistance(float voltage);
 };
 class DistanceSensor
 {
 	private:
 		int pin;
+		float coeficiente, exponente;
 	public:
 		DistanceSensor(int pinInput);
-		float analogVoltage();
-		float digitalVoltage();
+		float anRead();
+		float digRead();
 		float distance();
+		void setCoeficiente(float coef);
+		void setExponente(float exp);
+		float getCoeficiente();
+		float getExponente();
 };
 
 class LightSensor
@@ -26,8 +38,8 @@ class LightSensor
 		int pin;
 	public:
 		LightSensor(int pinInput);
-		float analogVoltage();
-		float digitalVoltage();
+		float anRead();
+		float digRead();
 		boolean black();
 };
 
@@ -40,6 +52,18 @@ public:
 	void forward();
 	void backward();
 	void stop();
+};
+
+class Led
+{
+private:
+	int pinRed, pinGreen, pinBlue;
+public:
+	Led(int red, int green, int blue);
+	void red();
+	void green();
+	void blue();
+	void off();
 };
 
 #endif
